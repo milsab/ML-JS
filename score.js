55720e4d24fa84f107099bfd7a58a0de1735f523
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2019-09-20 17:39:28 -0500

    Add Features

--- score.js
+++ score.js
@@ -25,7 +25,7 @@ function runAnalysis() {
   // Using Lodash
   _.range(1, 20).forEach(k => {
     const accuracy = _.chain(testSet)
-    .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+    .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
     .size()
     .divide(testSize)
     .value();
@@ -35,7 +35,12 @@ function runAnalysis() {
 
 function knn(data, point, k){
   return _.chain(data)
-  .map(row => [distance(row[0], point), row[3]])
+  .map(row => {
+    return [
+      distance(_.initial(row), point), 
+      _.last(row[3])
+    ];
+  })
   .sortBy(row => row[0])
   .slice(0, k)
   .countBy(row => row[1])
@@ -48,7 +53,11 @@ function knn(data, point, k){
 }
 
 function distance(pointA, pointB){
-  return Math.abs(pointA - pointB);
+  return _.chain(pointA)
+        .zip(pointB)
+        .map(([a, b]) => (a - b) ** 2) // sqr 2
+        .sum()
+        .value() ** 0.5; // sqrt 2 
 }
 
 function splitData(data, testCount){
